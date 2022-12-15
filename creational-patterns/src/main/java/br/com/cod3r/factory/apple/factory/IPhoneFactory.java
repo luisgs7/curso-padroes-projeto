de2803462a01dcfc3dd6f900e4d7e9b72cc92e7e
luisgs7 <luisgsilva270@gmail.com>
2022-12-15 15:42:24 -0300

    Factory after: Implemented IPhoneFactory

--- creational-patterns/src/main/java/br/com/cod3r/factory/apple/factory/IPhoneFactory.java
+++ creational-patterns/src/main/java/br/com/cod3r/factory/apple/factory/IPhoneFactory.java
@@ -1,6 +1,21 @@
 package br.com.cod3r.factory.apple.factory;
 
+import br.com.cod3r.factory.apple.model.IPhone;
+
 public abstract class IPhoneFactory {
 
-	
+	public IPhone orderIphone() {
+        IPhone device = null;
+
+        device = createIphone();
+
+        device.getHardware();
+        device.assemble();
+        device.certificates();
+        device.pack();
+
+        return device;
+    }
+
+    public abstract IPhone createIphone();
 }
